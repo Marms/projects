e40f1bb8dd126e23fa1e6371d19d082314b4cb5c
Florent Delmotte <f.delmotte@kiabi.com>
2017-10-25 13:39:14 +0200

    Initial commit

---
+++ src/test/java/com/example/TestSpringReactive/TestSpringReactiveApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.TestSpringReactive;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestSpringReactiveApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
